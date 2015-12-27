d48a59c26483fd2fdd02726b21a0f4b4bd5d520a
doodad-js <doodad-js@f4e9449a-2e7a-49bc-89ce-d1f85e87716c>
2015-12-27 18:17:17 +0000

    - <Fix> Tools.Url and Tools.Path - <Fix> Tools.safeEval: Unauthorized global variables can be acceded - <Fix> NodeJs.Server.Http: Was resolving on error - <Fix> Tools.Files.openFile: Was resolving on error - <Fix> Doodad.Tools.Locale.setLocale: Was resolving on error - <Fix> Doodad.Tools.Locale.setLocale: IE doesn't understand "utf8", but it understands "utf-8". - <Fix> Doodad.Tools.readFile (client).: Was resolving on error - <Fix> Terminal: "exit" and "quit" commands was no longer working (regression) - <Fix> Terminal: Error when command is only spaces - <Fix> Client: Missing "var" - <Fix> Tools.Config.loadFile: Multiple fixes - <Fix> "Set" polyfill: Constructor on array was not creating a Set with unique items - <New> NodeJs.Server.Http.StaticPage: Multi-paths - <New> Server.Http.Page: "$prepare" - <New> IO.Stream: Implement "pipe" and "unpipe". - <New> Doodad.Tools.Locale: New settings option "localePath" - <New> Promise.nodeify: Replaces previous "finally" polyfill - <New> Modules.load and Modules.locate - <Chg> Tools.Config: Client expects URLs, Server expects system paths. - <Chg> Loader: Rewritten with Promises - <Chg> IO.PipeStream and IO.TextPipeStream: Replaced by "IO.Stream.pipe". - <Chg> Project splited into separated packages - <Chg> Module skeleton: When server side, exports the function "add" and no longer use "global" to store modules in DD_MODULES. - <Chg> NodeJs.Server.Http.StaticPage: Rename "templateFolder" to "folderTemplate". - <Chg> Server.Http.JsonRpc.Page: "GET" now returns a JSON-RPC response like "POST". - <Chg> Bootstrap has its own options, moved from Types. - <Chg> Bootstrap no longer loads non-bootstrap namespaces. They must be loaded separately using "Namespaces.loadNamespaces". - <Chg> Namespaces: Rewritten with Promises - <Chg> Namespaces.loadNamespaces: Add new "callback" parameter, at position 0. - <Chg> Promise.finalize Polyfill: Works like Bluebird's finalize function. - <Chg> NodeJs.getPackageInfo removed. - <Chg> NodeJs.requireModule: Replaced by Modules.load - <Chg> NodeJs.resolveClientModule: Replaces by Modules.locate

---
+++ trunk/node_modules/doodad-js-make/make.js
@@ -0,0 +1,66 @@
+// dOOdad - Object-oriented programming framework with some extras
+// File: make.js - Make command
+// Project home: https://sourceforge.net/projects/doodad-js/
+// Trunk: svn checkout svn://svn.code.sf.net/p/doodad-js/code/trunk doodad-js-code
+// Author: Claude Petit, Quebec city
+// Contact: doodadjs [at] gmail.com
+// Note: I'm still in alpha-beta stage, so expect to find some bugs or incomplete parts !
+// License: Apache V2
+//
+//	Copyright 2015 Claude Petit
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//		http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+
+"use strict";
+
+const DD_MODULES = {};
+
+require('doodad-js-io').add(DD_MODULES);
+require('doodad-js-minifiers').add(DD_MODULES);
+require('doodad-js-make/index.js').add(DD_MODULES);
+
+const root = require('doodad-js').createRoot(DD_MODULES, {startup: {settings: {fromSource: true}}}),
+	doodad = root.Doodad,
+	tools = doodad.Tools,
+	types = doodad.Types,
+	namespaces = doodad.Namespaces;
+
+function startup() {
+	const make = root.Make;
+	
+	let command = (process.argv[2] || '').toLowerCase();
+	
+	if (['make', 'install', 'test', 'custom'].indexOf(command) < 0) {
+		console.error("Invalid command. Available commands are : 'make', 'install', 'test' and 'custom'.");
+		process.exit(1);
+	};
+
+	if (command === 'custom') {
+		const name = process.argv[3];
+		if (!name) {
+			console.error("Missing parameter to command 'custom'.");
+			process.exit(1);
+		};
+		command = name;
+	};
+	
+	make.run(command);
+};
+
+//process.stdin.on('data', function(){})
+
+namespaces.loadNamespaces(startup, false, null, DD_MODULES)
+	['catch'](function(err) {
+		console.error(err.stack);
+		process.exit(1);
+	});
